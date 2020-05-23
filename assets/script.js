3f0b52f713a9a2f01c4e11e3a4988e69efd7914c
John Bentley <jbentley1011@gmail.com>
2020-05-23 17:39:48 -0500

    because I keep forgetting to commit regularly

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,33 @@
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+$(document).ready(function () {
+    // live time clock
+    function update() {
+        $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }
+    
+    setInterval(update, 1000);
+
+    // specify what in HTML relates to current time
+    let currentHour = moment().hour();
+    let time = [
+        hour0800, hour0900, hour1000, hour1100, hour1200, 
+        hour1300, hour1400, hour1500, hour1600, hour1700, 
+        hour1800, hour1900, hour2000];
+    for (let i = 0; i < time.length; i++) {
+        if (currentHour < time[i]) {
+            $("#" + time[i]);
+            removeClass("present")
+            addClass("future");
+        }
+        
+        else if (currentHour > time[i]) {
+            $("#" + time[i])
+            removeClass("present")
+            addClass("past");
+        }
+    }
+});
+
+
+// locale storage
+$(".saveBtn").val(localStorage.getItem(""));
